429f8b410617e510413b380c9b37799fae1fbde7
theniconaze-crypto <the.niconaze@gmail.com>
2026-08-28 09:28:42 +0200

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,12 +1,21 @@
-export const firebaseConfig = {
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyB6gFF8uic6QMOnrHAvX7nyfs87gHgIxgg",
-  authDomain: "Suivi-ESP.firebaseapp.com",
-  projectId: "Suivi-ESP",
-  storageBucket: "Suivi-ESP.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:xxxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "suivi-esp.firebaseapp.com",
+  projectId: "suivi-esp",
+  storageBucket: "suivi-esp.firebasestorage.app",
+  messagingSenderId: "556406528167",
+  appId: "1:556406528167:web:e731bedb048ace7d262382",
+  measurementId: "G-DDVF575HZE"
 };
 
-// Détecte si la config ci-dessus est toujours le placeholder par défaut.
-export const firebaseConfigEstRenseignee =
-  firebaseConfig.apiKey !== "AIzaSyB6gFF8uic6QMOnrHAvX7nyfs87gHgIxgg" && !!firebaseConfig.projectId;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
